1469fa713e34911a96f304f1d557d761bdda930b
kevincms <kevincms314@naver.com>
2025-12-05 10:42:51 +0900

    spring 세팅

---
+++ miniproject4-spring/miniproject04/src/main/java/com/example/miniproject04/Entity/User.java
@@ -0,0 +1,24 @@
+package com.example.miniproject04.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long UserId;
+
+    @Column()
+    private String LoginId;
+
+
+}
